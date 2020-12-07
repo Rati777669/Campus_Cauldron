a8845f3bf8ed927f2c1623710dbb63cb81ad5656
mahima-rai <72219072+mahima-rai@users.noreply.github.com>
2020-12-07 21:09:56 +0530

    Add files via upload

---
+++ include/navbar.php
@@ -0,0 +1,31 @@
+<ul class="navbar-nav mr-auto">
+  <li class="nav-item">
+    <a class="nav-link" href="index.php">Home</a>
+  </li>
+  <li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+      Links
+    </a>
+    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+      <a class="dropdown-item" href="index.php#after-intro">Notices</a>
+      <a class="dropdown-item" href="index.php#after-notice">Gallery</a>
+      <a class="dropdown-item" href="index.php#after-gallery">Clubs and Cells</a>
+      <a class="dropdown-item" href="index.php#after-clubs">Events and Fests</a>
+      <a class="dropdown-item" href="index.php#after-fests">FAQs</a>
+      <a class="dropdown-item" href="index.php#after-questions">College Map</a>
+
+    </div>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">About</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="ask.php">Ask</a>
+  </li>
+  <li class="nav-item">
+        <a class="nav-link" href="suggested_questions.php">Questions asked</a>
+      </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Contact Us</a>
+  </li>
+</ul>
