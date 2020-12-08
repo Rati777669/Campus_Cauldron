13813c38eb9a5a225b2daebf2f23ea7d947af290
mahima-rai <72219072+mahima-rai@users.noreply.github.com>
2020-12-09 01:50:32 +0530

    Minor Change

--- index.php
+++ index.php
@@ -160,7 +160,7 @@
             ?>
               <h5 id="list-item-1"><?php echo $row["question"]; ?></h5>
               <p><?php echo $row["answer"] ?>
-                <br> Posted by <b><?php echo $row["ans_by"] ?></b></p>
+                <br> Answered by <b><?php echo $row["ans_by"] ?></b></p>
               <hr>
             <?php
             };
