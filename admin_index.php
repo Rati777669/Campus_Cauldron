33560cd8f0b4de12881c6eafcc04b4e8f6bd9ab4
Rati777669 <73477132+Rati777669@users.noreply.github.com>
2020-12-08 22:14:20 +0530

    Update admin_index.php

--- admin_index.php
+++ admin_index.php
@@ -55,10 +55,12 @@ include('include/security.php');
                     <div class="d-sm-flex align-items-center justify-content-between mb-4">
                         <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
                     </div>
+                    
 
                     <!-- Content Row -->
-
-                    <!-- End of Footer -->
+                    <div class="row"><?php
+ include 'include/dashboard.php' ?>
+                    
 
                 </div>
                 <!-- End of Content Wrapper -->
