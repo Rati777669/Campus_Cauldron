c910ca46d475e06d20b9695ba65e7fd2a5f68908
mahima-rai <72219072+mahima-rai@users.noreply.github.com>
2020-12-10 09:48:50 +0530

    Correction in title

--- view_clubs.php
+++ view_clubs.php
@@ -13,7 +13,7 @@ include('include/security.php');
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Question Panel</title>
+    <title>view-clubs</title>
 
     <!-- Custom fonts for this template-->
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
